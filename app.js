5c5869671bd8d797f55a102bce3422c1b9d17732
berreyilmaz <berre.yilmaz24@gmail.com>
2025-06-26 14:37:44 +0300

    template engine-pub

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
-const userRoutes = require('./routes/user');
 const path = require('path');
 
 app.set('view engine','pug');
 app.set('views','./views');
 
+const admin = require('./routes/admin');
+const userRoutes = require('./routes/user');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname,'public')));
 
 //routes
-app.use('/admin', adminRoutes);
+app.use('/admin', admin.routes);
 app.use(userRoutes);
 
 app.use((req,res)=>{
-    res.status(404).render('404');
+    res.status(404).render('404',{title:'Page Not Found'});
 });
 app.listen(3000, () => {
     console.log('listening on port 3000');
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+
+
+const products = [
+    {name:'İphone 6', price: 10000, image:'1.jpg', description:'iyi telefon'},
+    {name:'İphone 11', price: 20000, image:'2.jpeg', description:'eh işte'},
+    {name:'İphone 14 pro', price: 50000, image:'3.jpeg', description:'harika'},
+    {name:'Samsung A52', price: 30000, image:'4.jpeg', description:'müthiş'}
+]
 
 // /admin/add-product => GET
 router.get('/add-product',(req,res,next)=>{
-    res.render('add-product');
+    res.render('add-product', 
+        {
+            title:'Add a new Product',
+            path:'/admin/add-product'
+        });
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req,res,next)=>{
-    //database kayıt
-    console.log(req.body);
+    products.push({name: req.body.name, price: req.body.price, image: req.body.image, description: req.body.description});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
