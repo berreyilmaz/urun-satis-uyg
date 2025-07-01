a18fca80f693575c155804e7c48583411cbc9f6b
berreyilmaz <berre.yilmaz24@gmail.com>
2025-07-01 14:29:15 +0300

    models

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-
-const products = [
-    {name:'İphone 6', price: 10000, image:'1.jpg', description:'iyi telefon'},
-    {name:'İphone 11', price: 20000, image:'2.jpeg', description:'eh işte'},
-    {name:'İphone 14 pro', price: 50000, image:'3.jpeg', description:'harika'},
-    {name:'Samsung A52', price: 30000, image:'4.jpeg', description:'müthiş'}
-]
+const productsController = require('../controllers/products');
 
 // /admin/add-product => GET
-router.get('/add-product',(req,res,next)=>{
-    res.render('add-product', 
-        {
-            title:'Add a new Product',
-            path:'/admin/add-product'
-        });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req,res,next)=>{
-    products.push({name: req.body.name, price: req.body.price, image: req.body.image, description: req.body.description});
-    res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
