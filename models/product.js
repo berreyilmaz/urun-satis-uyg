2ffce60dc1a65da4fc66e49356432ae0c790456e
berreyilmaz <berre.yilmaz24@gmail.com>
2025-07-03 15:58:56 +0300

    ürün detay ve güncelleme sayfası

--- models/product.js
+++ models/product.js
@@ -1,7 +1,11 @@
-const products = [];
+const products = [{id:"13213", name:'Iphone 6',price:'10000', imageUrl:'1.jpeg',description:'iyi telefon'},
+    {id:"13214", name:'Iphone 11',price:'30000', imageUrl:'2.jpeg',description:'daha iyi telefon'},
+    {id:"13215", name:'Iphone 14 pro',price:'80000', imageUrl:'3.jpeg',description:'çok daha iyi telefon'}
+];
 
 module.exports=class Product {
     constructor(name, price, imageUrl, description) {
+        this.id = (Math.floor(Math.random()*99999)+1).toString();
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
@@ -15,6 +19,18 @@ module.exports=class Product {
     static getAll() {
         return products;
     }
-}
 
-// Product.getAll();
+    static getById(id) {
+        const product = products.find(i => i.id === id);
+        return product;
+    }
+
+    static Update(product) {
+        const index = products.findIndex(i=>i.id===product.id);
+
+        products[index].name = product.name;
+        products[index].price = product.price;
+        products[index].imageUrl = product.imageUrl;
+        products[index].description = product.description;
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
-// /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/products', adminController.getProducts);
 
-// /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/products/:productid', adminController.getEditProduct);
+
+router.post('/products', adminController.postEditProduct);
 
 module.exports = router;
